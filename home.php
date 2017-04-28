e840b3bb186decfdd19de8766b9aafbc5511032b
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2017-04-28 23:13:16 +0600

    Home page is added

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Online Election System </title>
+	</head>
+
+	<style type="text/css">
+		
+	body
+	{
+		background-color: gray;
+	}
+
+	</style>
+
+	<body>
+	<h1>Welcome Online Election System</h1>
+	<marquee> Welcome Bangladesh Election System Welcome Bangladesh Election System</marquee>
+	</body>
+</html>
